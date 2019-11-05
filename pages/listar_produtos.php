cec85aaf24436e4e186e24e4e7b253cbabc9f955
Emerson <emersonnnb@hotmail.com>
2019-11-05 20:04:26 -0300

    atualização_menu

--- pages/listar_produtos.php
+++ pages/listar_produtos.php
@@ -4,7 +4,7 @@ include 'validacao.php';
 <!DOCTYPE html>
 <html lang="pt-br">
 <title>Lista de Produtos</title>
-<body>    
+<body>
     <div style="padding:20px 0" class="container">
         <h3 style="margin-bottom:30px">Lista de Produtos</h3>
 
@@ -23,60 +23,59 @@ include 'validacao.php';
             <tbody>
 
                 <?php
-include_once 'conexao.php';
-$sql = "SELECT IdProduto, Categoria, Fornecedor, Nome, Numero, Quantidade FROM `estoque`";
-$retorno = mysqli_query($conexao, $sql);
+                include_once 'conexao.php';
+                $sql = "SELECT IdProduto, Categoria, Fornecedor, Nome, Numero, Quantidade FROM `estoque`";
+                $retorno = mysqli_query($conexao, $sql);
 
-while ($array = mysqli_fetch_array($retorno, MYSQLI_ASSOC)) {
-    $idProduto = $array['IdProduto'];
-    $categoria = $array['Categoria'];
-    $fornecedor = $array['Fornecedor'];
-    $nome = $array['Nome'];
-    $numero = $array['Numero'];
-    $quantidade = $array['Quantidade'];
-    ?>
-                <tr>
-                    <td><?=$idProduto?></td>
-                    <td><?=$nome?></td>
-                    <td><?=$numero?></td>
-                    <td><?=$categoria?></td>
-                    <td><?=$quantidade?></td>
-                    <td><?=$fornecedor?></td>
-                    <td>
-                        <?php if (($nivel == 1) || ($nivel == 2)) {?>
-                        <a class="btn-editar btn btn-sm btn-primary" href="editar_produto.php?id=<?=$idProduto?>"
-                            role="button"><i class="far fa-edit"></i> Editar</a>
-                        <?php }?>
-                        <?php if ($nivel == 1) {?>
-                        <a class="btn-editar btn btn-sm btn-danger" href="excluir_produto.php?id=<?=$idProduto?>"
-                            role="button"><i class="far fa-trash-alt"></i> Excluir</a></td>
-                    <?php }?>
-                </tr>
-                <?php }?>
+                while ($array = mysqli_fetch_array($retorno, MYSQLI_ASSOC)) {
+                    $idProduto = $array['IdProduto'];
+                    $categoria = $array['Categoria'];
+                    $fornecedor = $array['Fornecedor'];
+                    $nome = $array['Nome'];
+                    $numero = $array['Numero'];
+                    $quantidade = $array['Quantidade'];
+                    ?>
+                    <tr>
+                        <td><?= $idProduto ?></td>
+                        <td><?= $nome ?></td>
+                        <td><?= $numero ?></td>
+                        <td><?= $categoria ?></td>
+                        <td><?= $quantidade ?></td>
+                        <td><?= $fornecedor ?></td>
+                        <td>
+                            <?php if (($nivel == 1) || ($nivel == 2)) { ?>
+                                <a class="btn-editar btn btn-sm btn-primary" href="editar_produto.php?id=<?= $idProduto ?>" role="button"><i class="far fa-edit"></i> Editar</a>
+                            <?php } ?>
+                            <?php if ($nivel == 1) { ?>
+                                <a class="btn-editar btn btn-sm btn-danger" href="excluir_produto.php?id=<?= $idProduto ?>" role="button"><i class="far fa-trash-alt"></i> Excluir</a></td>
+                    <?php } ?>
+                    </tr>
+                <?php } ?>
 
             </tbody>
         </table>
 
         <?php
-if (isset($_GET['atualizado'])) {
-    echo '<div id="alerta" class="alert alert-success" role="alert">
+        if (isset($_GET['atualizado'])) {
+            echo '<div id="alerta" class="alert alert-success" role="alert">
                 Produto <b>' . $_GET['atualizado'] . '</b> atualizado com sucesso!.
                 </div>';
-}
-if (isset($_GET['excluido'])) {
-    echo '<div id="alerta" class="alert alert-danger" role="alert">
+        }
+        if (isset($_GET['excluido'])) {
+            echo '<div id="alerta" class="alert alert-danger" role="alert">
                 Produto <b>' . $_GET['excluido'] . '</b> excluido com sucesso!.
                 </div>';
-}
-if (isset($_GET['cadastrado'])) {
-    echo '<div id="alerta" class="alert alert-success" role="alert">
+        }
+        if (isset($_GET['cadastrado'])) {
+            echo '<div id="alerta" class="alert alert-success" role="alert">
                 Produto cadastrado com sucesso!.
                 </div>';
-}
-?>
+        }
+        ?>
     </div>
+  
 
-    <?php include_once '../footer.php'?>
+    <?php include_once '../footer.php' ?>
 </body>
 
 </html>
