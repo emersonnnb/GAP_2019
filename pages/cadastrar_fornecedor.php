aa8d79f8566fd5640e990febba93e3cc3d1df236
dayanneGomes <46797070+DayCrazy1@users.noreply.github.com>
2019-11-14 12:15:46 -0300

    Update cadastrar_fornecedor.php

--- pages/cadastrar_fornecedor.php
+++ pages/cadastrar_fornecedor.php
@@ -3,12 +3,22 @@
 <title>Cadastro de Fornecedor</title>
 <body>    
     <div style="padding:20px 0;max-width:800px" class="container">
+        
         <h4 style="padding:0 0 20px 0;margin-bottom:35px;" class="border-bottom">Cadastro de Fornecedor</h4>
         <form action="inserir_fornecedor.php" method="POST">
             <div class="form-group">
-                <label for="numeroProduto">Nome Fornecedor</label>
+                
+                <label for="idProduto">Nome Fornecedor</label>
+                
                 <input type="text" class="form-control" id="nomeFornecedor" placeholder="Digite o nome do fornecedor"
                     name="nomeFornecedor" required autocomplete="off">
+            </div>
+            
+            <div class="form-group">
+                <label for="cnpjFornecedor">CNPJ<label>
+                    
+                    <input type="text" class="form-control" id="cnpjFornecedor" placeholder="Digite o CNPJ do fornecedor"
+                           name="cnpjFornecedor" required atocomplete"on">
             </div>
             <button type="submit" class="btn-enviar btn btn-success btn-sm btn-block">Cadastrar</button>
         </form>
